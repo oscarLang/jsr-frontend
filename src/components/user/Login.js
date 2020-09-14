3778f4468d5831d2e697b65d3d81637fd15d342f
Oscar Lang <oscar.lang98@gmail.com>
2020-09-14 13:12:39 +0100

    added basic login and register view

---
+++ src/components/user/Login.js
@@ -0,0 +1,93 @@
+import React from "react";
+import axios from 'axios';
+
+import {
+  Grid,
+  Typography,
+  Avatar,
+  TextField,
+  FormControlLabel,
+  Checkbox,
+  Button,
+  Link,
+} from "@material-ui/core";
+
+function Login() {
+    const [form, changeForm] = React.useState({
+      email: "",
+      password: "",
+    });
+    function change(event) {
+        var name = event.target.name;
+        var value = event.target.value;
+        changeForm({ ...form, [name]: value });
+    }
+    function loginUser(event) {
+        console.log(form);
+      event.preventDefault();
+      axios({
+        method: 'post',
+        url: 'http://localhost:1337/user/login',
+        data: {
+          email: form.email,
+          password: form.password
+        }
+    })
+    .then(function (response) {
+        console.log(response);
+    });
+
+    }
+  return (
+    <Grid container>
+      <Grid item xs={12} sm={4}>
+        <div>
+          <Avatar />
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <form onSubmit={loginUser}>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+              value={form.email}
+              onChange={change}
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              value={form.password}
+              onChange={change}
+            />
+            <Button type="submit" fullWidth variant="contained" color="primary">
+              Sign In
+            </Button>
+            <Grid container>
+              <Grid item>
+                <Link href="#" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+          </form>
+        </div>
+      </Grid>
+    </Grid>
+  );
+}
+
+export default Login;
---
+++ src/components/user/Register.js
@@ -0,0 +1,155 @@
+import React from "react";
+import axios from 'axios';
+import {
+  Grid,
+  Typography,
+  TextField,
+  FormControlLabel,
+  Checkbox,
+  Button,
+  Link,
+} from "@material-ui/core";
+// import "./Home.css";
+function Register() {
+  const [form, changeForm] = React.useState({
+    userName: "",
+    email: "",
+    password: "",
+  });
+  const [error, setError] = React.useState({
+    userName: false,
+    email: false,
+    password: false,
+  });
+  const [helper, setHelperText] = React.useState({
+    userName: "",
+    email: "",
+    password: "",
+  });
+  function registerUser(event) {
+    event.preventDefault();
+    axios({
+      method: 'post',
+      url: 'http://localhost:1337/user/register',
+      data: {
+        email: form.email,
+        password: form.password
+      }
+    });
+  }
+
+  function validate(event) {
+    var toValidateName = event.target.name;
+    var toValidateValue = event.target.value;
+    switch (toValidateName) {
+      case "userName":
+        if (toValidateValue.length <= 2) {
+            setError({ ...error, [toValidateName]: true });
+            setHelperText({ ...helper, [toValidateName]: "Username must contain more than one character" });
+        } else {
+          setError({ ...error, [toValidateName]: false });
+          setHelperText({ ...helper, [toValidateName]: "" });
+
+        }
+        break;
+      case "email":
+        // eslint-disable-next-line
+        const re = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+        if (!re.test(toValidateValue)) {
+          setError({ ...error, [toValidateName]: true });
+          setHelperText({ ...helper, [toValidateName]: "Incorect format for email address" });
+        } else {
+          setError({ ...error, [toValidateName]: false });
+          setHelperText({ ...helper, [toValidateName]: "" });
+        }
+        break;
+      case "password":
+        // eslint-disable-next-line
+        const regPass = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/i;
+        if (!regPass.test(toValidateValue)) {
+          setError({ ...error, [toValidateName]: true });
+          setHelperText({ ...helper, [toValidateName]: "Password must contain a minimum of 8 characters, including on uppercase, one digit and one lowercase" });
+        } else {
+          setError({ ...error, [toValidateName]: false });
+          setHelperText({ ...helper, [toValidateName]: "" });
+        }
+        break;
+      default:
+        break;
+    }
+    //https://stackoverflow.com/questions/59813926/usestate-to-update-multiple-values-in-react
+    changeForm({ ...form, [toValidateName]: toValidateValue });
+  }
+
+  return (
+    <Grid container>
+      <Grid item xs={12} sm={2}>
+        <Typography component="h1" variant="h5">
+          Sign up
+        </Typography>
+        <form onSubmit={registerUser}>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <TextField
+                name="userName"
+                variant="outlined"
+                required
+                fullWidth
+                id="userName"
+                label="Username"
+                autoFocus
+                error={error.userName}
+                onChange={validate}
+                value={form.userName}
+                helperText={helper.userName}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                error={error.email}
+                onChange={validate}
+                value={form.email}
+                helperText={helper.email}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+                error={error.password}
+                onChange={validate}
+                value={form.password}
+                helperText={helper.password}
+              />
+            </Grid>
+          </Grid>
+          <Button type="submit" fullWidth variant="contained" color="primary">
+            Sign Up
+          </Button>
+          <Grid container justify="flex-end">
+            <Grid item>
+              <Link href="#" variant="body2">
+                Already have an account? Sign in
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </Grid>
+    </Grid>
+  );
+}
+
+export default Register;
