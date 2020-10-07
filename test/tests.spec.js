2fb0f5cbc1f6730154f75954f400a95fabc1b797
Oscar Lang <oscar.lang98@gmail.com>
2020-10-07 13:23:22 +0100

    maybe?

--- test/tests.spec.js
+++ test/tests.spec.js
@@ -61,27 +61,29 @@ describe('Login and reports', function() {
   it('Login and create new report on week 6', async function() {
     await driver.get("https://oscarlang.me/")
     await driver.manage().window().setRect(794, 824)
-    await driver.findElement(By.css(".MuiSvgIcon-root")).click()
+    await driver.get("https://oscarlang.me/login");
+    await driver.manage().window().setRect(794, 824);
+    await driver.findElement(By.id("email")).then(function(el) {
+        el.click();
+        el.sendKeys("e2e@test.com");
+    });
+    await driver.findElement(By.id("password")).then(function(el) {
+        el.click();
+        el.sendKeys("Test1234");
+    });
+    await driver.findElement(By.id("signin")).click();
     await driver.wait(
-        until.elementLocated(By.linkText("Log in")),
+        until.elementLocated(By.id("home")),
         5000
     );
-    await driver.findElement(By.linkText("Log in")).click()
-    await driver.findElement(By.css(".MuiBackdrop-root")).click()
-    await driver.findElement(By.id("email")).click()
-    await driver.findElement(By.id("email")).sendKeys("e2e@test.com")
-    await driver.findElement(By.id("password")).click()
-    {
-      const element = await driver.findElement(By.css(".MuiButton-root"))
-      await driver.actions({ bridge: true }).move(element).perform()
-    }
-    await driver.findElement(By.id("password")).sendKeys("Test1234")
-    await driver.findElement(By.css(".MuiButton-root")).click()
-    {
-      const element = await driver.findElement(By.css(".MuiSvgIcon-root"))
-      await driver.actions({ bridge: true }).move(element).perform()
-    }
+    driver.getCurrentUrl().then(function(url) {
+        assert.ok(url.endsWith("/"));
+    });
     await driver.findElement(By.css(".MuiSvgIcon-root")).click()
+    await driver.wait(
+        until.elementLocated(By.id("logout")),
+        5000
+    );
     await driver.wait(
         until.elementLocated(By.linkText("Reports")),
         5000
@@ -97,6 +99,7 @@ describe('Login and reports', function() {
 
     await driver.findElement(By.id("week")).click();
     {
+
         const element = await driver.findElement(By.xpath("//*[normalize-space(text()) = '6']"));
         await driver.actions({ bridge: true }).move(element).click(element).perform();
     }
